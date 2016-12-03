71cd6dfde77c76830303a442b6b3fbdcbb735aad
Wayne lin <wayne_lin@maxense.com.tw>
2016-12-03 20:22:16 +0800

    random tips

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,4 +6,17 @@ module HomeHelper
       "抱歉你答錯了！"
     end
   end
+
+  def get_tip(level_id)
+    level = Level.find(level_id)
+    tips = level.tips
+    count = tips.count
+    if count == 0
+      "抱歉！ 本題尚無任何提示"
+    else
+      prng = Random.new
+      index = prng.rand(count)
+      "小提示： " + tips[index].name
+    end
+  end
 end
